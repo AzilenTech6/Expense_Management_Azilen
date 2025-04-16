5f3ac6926c89fda9405f39ea370848fe3870aa97
Mohd Gayasudddin <mohd.gayasuddin@azilen.com>
2025-04-16 10:27:25 +0530

    added crud apis

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,19 +2,29 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { BudgetsModule } from './modules/budgets/budgets.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { ExpensesModule } from './modules/expenses/expenses.module';
+import { ReportsModule } from './modules/reports/reports.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      port: 5432,
+      port: 5433,
       username: 'postgres',
       password: 'postgres',
       database: 'expense_management',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true, // Set to false in production
     }),
+    AuthModule,
+    CategoriesModule,
+    BudgetsModule,
+    ExpensesModule,
+    ReportsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
